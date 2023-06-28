c64c00f36f857372f9560d0eef72833669ef5f58
HappyYama <milesdanieldragon@gmail.com>
2023-06-28 17:42:45 +0530

    improvements to js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
 // Active section manager
-let navLinks = document.querySelectorAll('header nav a');
-let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll("section");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        }
-    });
-};
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
 
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+  // toggle icon navbar
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = function () {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.onclick = function () {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  };
+
+  // remove navbar when a nav link is clicked
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
